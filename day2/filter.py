2f0ca53ca4542ece604e59a04075d61a0ba9944a
bconnor5 <bconnor5@jhu.edu>
2015-08-25 11:24:43 -0400

    Filter lines 10-15

--- day2/filter.py
+++ day2/filter.py
@@ -4,9 +4,27 @@ filename = "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 
 f = open ( filename )
 
-for line in f:
-    fields = line.split() 
+#line_count=0 #alternatively use enumerate (f) as " for line_count, data in enumerate (f) "
+#for line in f:
+    #fields = line.split() 
         
-    if "tRNA" in fields[9]:
-        print line, #comma suppresses new line
-      
+    #if "tRNA" in fields[9]:
+     #   print line, #comma suppresses new line
+ #   if line_count <= 10:
+  #      pass
+   # elif line_count <= 15:
+    #    print line,
+    #else:
+     #     break
+    #line_count += 1
+
+
+#alternate method
+
+for line_count, line in enumerate(f):
+      if line_count <=10:
+            pass
+      elif line_count <=15:
+            print line
+      else:
+            break
