b5f82197f20de912b7ed950d3ec26a4d887c4223
bconnor5 <bconnor5@jhu.edu>
2015-08-25 11:08:56 -0400

    Added filter.py

---
+++ day2/filter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+filename = "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
+
+f = open ( filename )
+
+for line in f:
+    fields = line.split() 
+        
+    if "tRNA" in fields[9]:
+        print line, #comma suppresses new line
+      
