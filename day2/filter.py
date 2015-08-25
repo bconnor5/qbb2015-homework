a173c169d29623c6b1b722dc70a2c238a915d70c
bconnor5 <bconnor5@jhu.edu>
2015-08-25 12:13:58 -0400

    name_counts update

--- day2/filter.py
+++ day2/filter.py
@@ -3,6 +3,7 @@
 filename = "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 
 f = open ( filename )
+name_counts = {}
 
 #line_count=0 #alternatively use enumerate (f) as " for line_count, data in enumerate (f) "
 #for line in f:
@@ -21,10 +22,23 @@ f = open ( filename )
 
 #alternate method
 
-for line_count, line in enumerate(f):
-      if line_count <=10:
-            pass
-      elif line_count <=15:
-            print line
-      else:
-            break
+#for line_count, line in enumerate(f):
+ #     if line_count <=10:
+  #          pass
+   #   elif line_count <=15:
+    #        print line
+     # else:
+      #
+      
+      #      break
+for line_count, data in enumerate(f):
+    fields = data.split()
+    gene_name = fields[9]
+    if gene_name not in name_counts:
+        name_counts[ gene_name ] = 1
+    else: 
+        name_counts[ gene_name ] += 1
+                
+      # iterate key, value pairs from name counts dictionary
+for key, value in name_counts.iteritems():
+      print key, value
