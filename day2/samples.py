89161232544ff9426ab1c72dad9fec5f2d5ee935
bconnor5 <bconnor5@jhu.edu>
2015-08-26 07:52:14 -0400

    Answer and code for number 3 added

--- day2/samples.py
+++ day2/samples.py
@@ -1,27 +1,62 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 annotation = "/Users/cmdb/qbb2015/rawdata/samples.csv"
 df = pd.read_csv(annotation, header=0, comment=None)
+#call = "/Users/cmdb/qbb2015/stringtie/SRR072*/t_data.ctab"
 
-df.columns = ["sample", "sex", "stage"]
+#df.columns = ["sample", "sex", "stage"]
 
 #print df
 #Load the file /Users/cmdb/qbb2015/rawdata/samples.csv. Loop through these 16 samples and load the corresponding t_data.ctab file in /Users/cmdb/qbb2015/stringtie. Print out out the row for transcript FBtr0331261.
 
-
-roi = df["sample"].str.contains("SRR072*")
+#roi = df["sample"].str.contains("SRR072*")
 
 #print roi
-for filename in roi:
-      if roi == True:
-            print "Users/cmdb/qbb2015/stringtie/SRR072*/t_data.ctab"
+count = 0
+for filename in df["sample"]:
+      temp_loc = "/Users/cmdb/qbb2015/stringtie/"+filename+"/t_data.ctab"
+      file = open (str(temp_loc))
+      print filename
+      for line in file:
+            if "FBtr0331261" in line:
+                  print line
+                  count +=1
+            else:
+                  pass
+print count
+            #print "This is less frustrating"
+           #  call
+      #print filename
+      #print "okay"
+
+
+
+
+
+#print roi
+
+#for filename in roi:
+ #     print filename
+  #    print "odd"
+
+#for filename in roi:
+ #     if filename.endswith(89*):
+  #          print "choose"
+   #   elif filename.endswith(9**):
+    #        print "choose"
+     # else:
+      #      pass
+
+      # "Users/cmdb/qbb2015/stringtie/SRR072*/t_data.ctab"
             #open "Users/cmdb/qbb2015/stringtie/SRR072*/t_data.ctab"
             
-      else:
-            pass
+   
+   #   else:
+    
+    #        pass
       
      
   
