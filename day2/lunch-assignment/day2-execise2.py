0c65d4f2fffdeac6b97d62bb7e2839153c10404f
bconnor5 <bconnor5@jhu.edu>
2015-08-30 15:01:44 -0400

    answer and code for number 2

---
+++ day2/lunch-assignment/day2-execise2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+filename = "/Users/cmdb/qbb2015/day2-lunch/SRR072893.sam"
+
+f = open ( filename )
+
+perfect_match_count = 0
+for line in f:
+      if "@" in line:
+            pass
+      
+      elif "NM:i:0":
+            
+            perfect_match_count += 1
+      
+      else:
+            pass
+      
+
+print "Perfect Match Count: ", perfect_match_count
